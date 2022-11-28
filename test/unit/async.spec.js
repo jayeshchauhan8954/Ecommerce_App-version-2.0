5231c673f852844f8a1814b84bfc4912507f6e94
Jayesh Chauhan <officialjitandrachauhan@gmail.com>
2022-11-28 18:42:18 +0530

    ecomm_app_v-2.0

--- test/unit/async.spec.js
+++ test/unit/async.spec.js
@@ -1,18 +1,27 @@
+let promiseFunc = () => {
+    return new Promise((resolve, reject) => {
+        // resolve("I am a promise");
+        reject("I was rejected");
+    });
+};
 
-// let promiseFunction = () => {
-//     return new Promise((resolve, reject) => {
-//         resolve("I am a promise");
-//     });
-// };
+describe("Async JS", () => {
+    test("test your promise", () => {
+        promiseFunc()
+            .then((data) => {
+                expect(data).toBe("I am a promise");
+            })
+            .catch((err) => {
+                expect(err).toBe("I was rejected");
+            });
+    });
 
-// describe("Async JS", () => {
-//     test("test your promise", () => {
-//         promiseFunction().then((data) => {
-//             expect(data).toBe("I am a promise");
-//         }).catch((err) => {
-//             expect(err).toBe("I was rejected");
-
-//         })
-//     });
-
-// });
+    test("test your async/await", async () => {
+        try {
+            let output = await promiseFunc();
+            expect(output).toBe("I am a promise");
+        } catch (err) {
+            expect(err).toBe("I was rejected");
+        }
+    });
+});
--- test/unit/mock.spec.js
+++ test/unit/mock.spec.js
@@ -1,20 +1,43 @@
-let calculateEmailSent = (n, sendEmail) => {
-    let totalEmailSend = 0;
-    for (let i = 0; i < 5; i++) {
+let checkIfSystemWorking = (n, sendEmail) => {
+    let isSystemWorking = false;
+    for (let i = 0; i < n; i++) {
         let delivery = sendEmail();
-        totalEmailSend = delivery.passed >= delivery.failed;
+        isSystemWorking = delivery.passed > delivery.failed;
     }
-    return totalEmailSend;
-}
 
-describe("mock functions", () => {
+    return isSystemWorking;
+};
+
+describe("Mock functions", () => {
     let delivery = {
-        passed: 3,
-        failed: 2
-    }
+        passed: 2,
+        failed: 3,
+    };
     let sendEmail = jest.fn().mockReturnValue(delivery);
 
     test("first mock example", () => {
-        expect(calculateEmailSent(5, sendEmail)).toBe(true);
-    })
-})
+        expect(checkIfSystemWorking(5, sendEmail)).toBe(true);
+    });
+});
+
+
+// let calculateEmailSent = (n, sendEmail) => {
+//     let totalEmailSend = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let delivery = sendEmail();
+//         totalEmailSend = delivery.passed >= delivery.failed;
+//     }
+//     return totalEmailSend;
+// }
+
+// describe("mock functions", () => {
+//     let delivery = {
+//         passed: 3,
+//         failed: 2
+//     }
+//     let sendEmail = jest.fn().mockReturnValue(delivery);
+
+//     test("first mock example", () => {
+//         expect(calculateEmailSent(5, sendEmail)).toBe(true);
+//     })
+// })
